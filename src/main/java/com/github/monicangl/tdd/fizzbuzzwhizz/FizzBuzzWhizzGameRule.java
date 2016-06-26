e9f682da045f2bc1dfc63066b40f223713ec92d4
Guiling <monica_n88@163.com>
2016-06-26 14:06:34 +0800

    Refactor FizzBuzzWhizzGame with 'Extract Class' method, extract a new class FizzBuzzWhizzGameRule

---
+++ src/main/java/com/github/monicangl/tdd/fizzbuzzwhizz/FizzBuzzWhizzGameRule.java
@@ -0,0 +1,14 @@
+package com.github.monicangl.tdd.fizzbuzzwhizz;
+
+public class FizzBuzzWhizzGameRule {
+    public String handleNumber(int number) {
+        return fizzBuzzHandler.handleNumber(number);
+    }
+
+    private BaseHandler whizzHandler = new WhizzHandler(null);
+    private BaseHandler buzzHandler = new BuzzHandler(whizzHandler);
+    private BaseHandler fizzHandler = new FizzHandler(buzzHandler);
+    private BuzzWhizzHandler buzzWhizzHandler = new BuzzWhizzHandler(fizzHandler);
+    private FizzWhizzHandler fizzWhizzHandler = new FizzWhizzHandler(buzzWhizzHandler);
+    private FizzBuzzHandler fizzBuzzHandler = new FizzBuzzHandler(fizzWhizzHandler);
+}
